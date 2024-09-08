be189284d6f432d2e6cc3b1696823f939b6aeb2e
kurtvicentina <kerteee32@gmail.com>
2024-09-08 12:49:07 +0800

    v1

---
+++ src/app/domController.js
@@ -0,0 +1,118 @@
+import 'boxicons'
+
+const todayContainer = document.querySelector('.today-container')
+const todayWeatherContainer = todayContainer.querySelector('#todayWeather')
+const locationTitle = document.querySelector('#locationTitle')
+const descriptionTitle = document.querySelector('#descriptionTitle')
+const sunnyTitle = document.querySelector('#sunnyTitle')
+const rainyTitle = document.querySelector('#rainyTitle')
+const snowyTitle = document.querySelector('#snowyTitle')
+const feelTitle = document.querySelector('#feelsLike')
+const sunrise = document.querySelector('#sunrise')
+const sunset = document.querySelector('#sunset')
+const alertTitle = document.querySelector('#alertTitle')
+const weekContainer = document.querySelector('.week-container')
+const farenheitSpan = document.querySelector('#farenheit')
+const celciusSpan = document.querySelector('#celcius')
+const errorContainer = document.querySelector('#fail')
+const successContainer = document.querySelector('#success')
+const loaderContainer = document.querySelector('#loaderContainer')
+
+export async function todayDetails(todayWeather, location, description,sunny, rainy, snowy, feelsLike, rise, set, alert){
+    errorContainer.classList.add('hidden')
+    successContainer.classList.remove('hidden')
+    todayWeatherContainer.classList.add(`today-${todayWeather}`)
+    locationTitle.textContent = location
+    descriptionTitle.textContent = `${description}`
+    sunnyTitle.textContent = `Sun chances: ${sunny}`
+    rainyTitle.textContent = `Rain chances: ${rainy}`
+    snowyTitle.textContent = `Snow chances: ${snowy}`
+    feelTitle.textContent = `Temp feels like: ${feelsLike}`
+    sunrise.textContent = `Sunrise: ${rise}`
+    sunset.textContent = `Sunset: ${set}`
+    alertTitle.textContent = `${alert}`
+}
+
+
+export async function createWeekCards(day, weather, temperature, sunChance, rainChance){
+    deleteWeekCards()
+
+    const dayContainer = document.createElement('div')
+    dayContainer.setAttribute('class', 'day')
+
+    const dayTitle = document.createElement('h2')
+    dayTitle.setAttribute('class', 'day-title')
+
+    const weatherIcon = document.createElement('i')
+    weatherIcon.classList.add('weather-icon')
+
+    const dayTemperature = document.createElement('p')
+    dayTemperature.setAttribute('class', 'temperature')
+
+    const chanceOfSun = document.createElement('p')
+    chanceOfSun.setAttribute('class', 'chance')
+
+    const chanceOfRain = document.createElement('p')
+    chanceOfRain.setAttribute('class', 'chance')
+
+    dayTitle.textContent = day
+
+    if(await weather == 'sunny'){
+        weatherIcon.setAttribute('class', 'bx bxs-sun')
+        dayContainer.classList.add('sunny')
+    }else if(await weather == 'rainy'){
+        weatherIcon.setAttribute('class', 'bx bxs-cloud-rain')
+        dayContainer.classList.add('rainy')
+    }
+    else{
+        weatherIcon.setAttribute('class', 'bx bx-cloud-snow')
+        dayContainer.classList.add('snowy')
+    } 
+    dayTemperature.textContent = `The temperature will feel like: ${temperature}`
+    chanceOfSun.textContent = `Sun chance: ${sunChance}`
+    chanceOfRain.textContent = `Rain chance: ${rainChance}`
+
+    dayContainer.append(dayTitle)
+    dayContainer.append(weatherIcon)
+    dayContainer.append(dayTemperature)
+    dayContainer.append(chanceOfSun)
+    dayContainer.append(chanceOfRain)
+    weekContainer.append(dayContainer)
+}
+
+export function deleteWeekCards(){
+    weekContainer.innerHTML = ''
+}
+
+export function toggleMetrics(){
+    let active
+
+    if(farenheitSpan.classList.contains('span-is-active')){
+        farenheitSpan.classList.remove('span-is-active')
+        celciusSpan.classList.add('span-is-active')
+        active = 'metric'
+    }else{
+        celciusSpan.classList.remove('span-is-active')
+        farenheitSpan.classList.add('span-is-active')
+        active = 'us'
+    }
+    return active
+}
+
+export function displayError(error){
+    successContainer.classList.add('hidden')
+    errorContainer.classList.remove('hidden')
+
+    const errorMessage = errorContainer.querySelector('#failMessage')
+
+    errorMessage.textContent = error
+    errorContainer.append(errorMessage)
+}
+
+export function showLoader(){
+    loaderContainer.setAttribute('class', 'loader')
+}
+
+export function hideLoader(){
+    loaderContainer.setAttribute('class', 'hidden')
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,111 @@
 import './style.css'
 import 'boxicons'
 
+import { format } from 'date-fns'
 import {fetchWeatherApi} from './app/fetchWeatherApi'
+import {todayDetails, createWeekCards, toggleMetrics, displayError, showLoader, hideLoader } from './app/domController'
 
-async function getWeather(location) {
-    const weather = await fetchWeatherApi(location);
-    console.log(weather);
+const searchForm = document.querySelector('#searchForm')
+const searchInput = document.querySelector('#searchInput')
+const searchBtn = document.querySelector('#searchBtn')
+const metricBtn = document.querySelector('#metricBtn')
+const currentLocation = document.querySelector('#locationTitle')
+let activeMetric
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    getWeather(searchInput.value, activeMetric)
+})
+
+metricBtn.addEventListener('click', () => {
+    let metricUsed = toggleMetrics()
+    if(!searchInput.value){
+        getWeather(currentLocation.textContent, metricUsed)
+    }else{
+        if(metricUsed == 'metric'){
+            getWeather(searchInput.value, metricUsed)
+        }else{
+            getWeather(searchInput.value, metricUsed)
+        }
+    }
+})
+
+function getMax(sun, rain, snow) {
+    let max ='';
+    
+    if (sun > rain && sun > snow) {
+        max = 'sunny';
+    } else if (rain > sun && rain > snow) {
+        max = 'rainy';
+    } else {
+        max = 'snowy';
+    }
+
+    return max;
 }
+
+async function changeDomMetric(metricPassed){
+    let metricSymbol
+    if(metricPassed == 'metric'){
+        metricSymbol = '°C'
+    } else{
+        metricSymbol = '°F'
+    }
+    return metricSymbol
+}
+
+async function getWeather(location, metric) {
+    try{
+        showLoader()
+        const metricSymbol = await changeDomMetric(metric)
+        const weather = await fetchWeatherApi(location, metric);
+        console.log(weather);
+        const rawlocationTitle = weather.weatherData.resolvedAddress
+        const locationTitle = rawlocationTitle.toString()
+        const description = weather.weatherData.days[0].description
+        const sunnyValue = 100 - weather.weatherData.days[0].cloudcover
+        const rainyValue = weather.weatherData.days[0].precipprob
+        const snowyValue = weather.weatherData.days[0].snow
+        const sunriseRaw = (weather.weatherData.days[0].sunrise).split(':')
+        const sunsetRaw = (weather.weatherData.days[0].sunset).split(':')
+        
+        const sunny = `${(100 - weather.weatherData.days[0].cloudcover).toFixed(2)}%`
+        const rainy = `${weather.weatherData.days[0].precipprob}%`
+        const snowy = `${weather.weatherData.days[0].snow}%`
+        const feel = `${weather.weatherData.days[0].feelslike}${metricSymbol}`
+        const rise = `${sunriseRaw[0]}:${sunriseRaw[1]}AM`
+        const set = `${sunsetRaw[0]}:${sunsetRaw[1]}PM`
+        const alert = weather.weatherData.alert || 'No alert for today'
+        let todayWeather = getMax(sunnyValue, rainyValue, snowyValue)
+
+        await todayDetails(todayWeather, locationTitle, description, sunny, rainy, snowy, feel, rise, set, alert)
+
+        for(let i = 0; i < 7; i++){
+            const sunny = 100 - weather.weatherData.days[i].cloudcover
+            const rainy = weather.weatherData.days[i].precipprob
+            const snowy = weather.weatherData.days[i].snow
+            let dayWeather = getMax(sunny, rainy, snowy)
+            const date = weather.weatherData.days[i].datetime
+            const dayName = format(date, 'EEEE')
+            const temperature = `${weather.weatherData.days[i].feelslike}${metricSymbol}`
+            const sunChance = `${(100 - weather.weatherData.days[i].cloudcover).toFixed(2)}%`
+            const rainChance = `${weather.weatherData.days[i].precipprob}%`
+
+            createWeekCards(dayName, dayWeather, temperature, sunChance, rainChance)
+        }
+    }catch(error){
+        displayError('Can\'t find location, please enter valid City or Country')
+        console.log(error)
+    }finally{
+        hideLoader()
+    }
+}
+
+activeMetric = toggleMetrics()
+getWeather('London', activeMetric)
+
+
+// THINGS TO DO
+
+// FONT AND LOGO DESIGN
